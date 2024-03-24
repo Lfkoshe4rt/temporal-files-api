7576229c9b38b9e3366fccfec2006fc0e10ed894
Marcelo <marcheloomeneses1@gmail.com>
2024-03-24 04:44:50 -0300

    Add onePrivateFile route to file.ts

--- src/routes/file.ts
+++ src/routes/file.ts
@@ -4,12 +4,15 @@ import {
   downloadFile,
   oneFile,
   uploadFile,
+  onePrivateFile,
 } from "../controllers/file";
 import { upload } from "../middlewares/fileUpload";
 
 const router = Router();
 
 router.post("/upload", upload.single("file"), uploadFile);
+
+router.post("/file/:id", onePrivateFile);
 
 router.get("/download/:id", downloadFile);
 
