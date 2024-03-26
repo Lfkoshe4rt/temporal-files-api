8ddf8f99414f93eff7437c0f5aa32175410c64e3
Marcelo <marcheloomeneses1@gmail.com>
2024-03-26 17:11:59 -0300

    Add error handling for file not found at path and invalid ID

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
 
 export const handleFileNotFoundError = (res: Response) => {
   return res.status(404).json({ status: "ERROR", message: "File not found" });
@@ -8,6 +9,17 @@ export const handleUnauthorizedError = (res: Response) => {
   return res
     .status(401)
     .json({ status: "ERROR", message: "Unauthorized access" });
+};
+
+export const handleFileNotFoundAtPath = (res: Response) => {
+  return res
+    .status(404)
+    .json({ status: "ERROR", message: "File not found at path" });
+};
+
+export const handleInvalidIdError = (res: Response, error: Error) => {
+  console.log(error);
+  return res.status(400).json({ status: "ERROR", message: "Invalid ID" });
 };
 
 export const handleInternalServerError = (res: Response, error: Error) => {
@@ -27,6 +39,7 @@ export const handleUnknownError = (res: Response, error: any) => {
 const errorHandlers: Record<string, (res: Response) => Response> = {
   "File not found": handleFileNotFoundError,
   "Unauthorized access": handleUnauthorizedError,
+  "File not found at path": handleFileNotFoundAtPath,
 };
 
 export const errorHandlerMiddleware = (
@@ -35,6 +48,10 @@ export const errorHandlerMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
+  if (error instanceof mongoose.Error.CastError && error.path === "_id") {
+    return handleInvalidIdError(res, error);
+  }
+
   if (error instanceof Error) {
     const handler = errorHandlers[error.message];
     if (handler) {
@@ -42,5 +59,6 @@ export const errorHandlerMiddleware = (
     }
     return handleInternalServerError(res, error);
   }
+
   return handleUnknownError(res, error);
 };
